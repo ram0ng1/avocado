3ca153e649f322d967ffaf0b3380eafe13d5b6b9
Ramon <ramon@ramonguilherme.com.br>
2026-05-23 21:09:01 -0300

    fix

--- js/src/forum/components/TagPage.tsx
+++ js/src/forum/components/TagPage.tsx
@@ -247,11 +247,16 @@ export default class AvocadoTagPage extends Page {
               />
             ))}
             {isLoadingNext && renderThreadSkeleton()}
-            {!isLoadingNext && discussions.length === 0 && !isInitialLoading && renderEmpty(trans('ramon-avocado.forum.tags.empty', 'No discussions in this category yet.'))}
+            {!isLoadingNext &&
+              discussions.length === 0 &&
+              !isInitialLoading &&
+              renderEmpty(trans('ramon-avocado.forum.tags.empty', 'No discussions in this category yet.'))}
             {isInitialLoading && discussions.length === 0 && renderThreadSkeleton()}
           </div>
 
-          {this.feedState?.hasNext() && !isLoadingNext && renderLoadMore(trans('ramon-avocado.forum.tags.load_more', 'Load more'), () => this.feedState.loadNext())}
+          {this.feedState?.hasNext() &&
+            !isLoadingNext &&
+            renderLoadMore(trans('ramon-avocado.forum.tags.load_more', 'Load more'), () => this.feedState.loadNext())}
         </div>
       </div>
     );
